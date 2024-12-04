e94559c38e9420b3280f81ecec9b273cc0632df3
Brie S. <35549267+coderBrie@users.noreply.github.com>
2024-12-04 14:42:35 -0600

    Update config.js
    removed userpool ARN and added user pool client ID to fix broken Cognito connection

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_KoBsFjw5L', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'arn:aws:cognito-idp:us-east-1:448049812628:userpool/us-east-1_KoBsFjw5L', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4t329uqf3v8jr4le3jq9rvallf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
